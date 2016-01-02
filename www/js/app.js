fc270d4246a62561b5332a872353d3604050294b
ashton kinslow <github@ashtonkinslow.com>
2016-01-01 20:29:08 -0600

    extracted routes.js

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.routes'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,81 +20,4 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.additem', {
-      url: '/additem',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/additem.html',
-        }
-      }
-    })
-  
-    .state('app.itemmatched', {
-      url: '/itemmatched',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/itemmatched.html',
-        }
-      }
-    })
-  
-    .state('app.myitem', {
-      url: '/myitem',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/myitem.html',
-        }
-      }
-    })
-  
-    .state('app.inventory', {
-      url: '/inventory',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/inventory.html',
-        }
-      }
-    })
-  
-    .state('app.chat', {
-      url: '/chat',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/chat.html',
-        }
-      }
-    })
-  
-    .state('app.settings', {
-      url: '/settings',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/settings.html',
-        }
-      }
-    })
-
-;
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/additem');
 });
