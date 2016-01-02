b830cf5776c1b24237f3b1ace261f3781debf16d
Gil Gildner <gil@canticlecreative.com>
2016-01-01 20:10:50 -0600

    added views

--- www/js/app.js
+++ www/js/app.js
@@ -32,15 +32,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
   .state('app.browse', {
       url: '/browse',
       views: {
@@ -57,16 +48,53 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+  
+    .state('app.itemmatched', {
+      url: '/itemmatched',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/itemmatched.html',
+        }
       }
-    }
-  });
+    })
+  
+    .state('app.myitem', {
+      url: '/myitem',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/myitem.html',
+        }
+      }
+    })
+  
+    .state('app.inventory', {
+      url: '/inventory',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/inventory.html',
+        }
+      }
+    })
+  
+    .state('app.chat', {
+      url: '/chat',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chat.html',
+        }
+      }
+    })
+  
+    .state('app.settings', {
+      url: '/settings',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/settings.html',
+        }
+      }
+    })
+
+;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/additem');
 });
